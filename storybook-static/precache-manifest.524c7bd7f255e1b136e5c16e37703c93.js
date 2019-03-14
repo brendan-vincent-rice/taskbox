4eb2aee52009c2c92b6d87955ced31cf75aadd19
Rice, Brendan <brice@cardinalsolutions.com>
2019-03-14 14:11:48 -0400

    taskbox UI

---
+++ storybook-static/precache-manifest.524c7bd7f255e1b136e5c16e37703c93.js
@@ -0,0 +1,19 @@
+self.__precacheManifest = [
+  {
+    "url": "vendors~main.986df1b324e650fa18ce.bundle.js"
+  },
+  {
+    "url": "runtime~main.986df1b324e650fa18ce.bundle.js"
+  },
+  {
+    "revision": "727bf7860cb200c2afc2",
+    "url": "main.css"
+  },
+  {
+    "url": "main.986df1b324e650fa18ce.bundle.js"
+  },
+  {
+    "revision": "cede25ada3042d4a841b8537c3e9da67",
+    "url": "iframe.html"
+  }
+];
